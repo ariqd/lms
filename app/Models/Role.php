912d4c6560c7284839629a78c0d33a72c594ed87
ariqd <ariqdaffaathallah@gmail.com>
2025-06-09 15:36:41 +0700

    fix: Fix unable to register

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -2,14 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
+    use HasFactory;
+
     protected $fillable = ['name', 'identity'];
 
     public function users()
     {
-        return $this->belongsTo(User::class);
+        return $this->hasMany(User::class);
     }
 }
